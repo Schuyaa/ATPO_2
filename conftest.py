bcfd616f6c90a6d83b217ec98ccdc36e53900e35
schulya <96183796+Schuyaa@users.noreply.github.com>
2025-04-22 22:43:40 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+# Обработка параметра language из командной строки
+def pytest_addoption(parser):
+    parser.addoption(
+        '--language', action='store', default='en', help="Choose the language for the tests"
+    )
+
+
+# Фикстура для запуска браузера с выбранным языком
+@pytest.fixture
+def browser(request):
+    language = request.config.getoption('language')
+    options = Options()
+    options.add_argument(f'--lang={language}')  # Устанавливаем язык интерфейса браузера
+    print(f"\nSetting up browser with language: {language}")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nTearing down browser...")
+    browser.quit()
 
